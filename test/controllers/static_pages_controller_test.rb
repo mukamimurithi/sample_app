899bc6131a7082fd45f3fb4805edf09a8935ca9d
carol <carolmurithi1994@gmail.com>
2016-07-28 12:12:59 +0300

    create signup page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,10 +3,12 @@ class StaticPagesControllerTest < ActionController::TestCase
 test "should get home" do
 get :home
 assert_response :success
+assert_select "title",  "Ruby on Rails Tutorial Sample App"
 end
 test "should get help" do
 get :help
 assert_response :success
+assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
 end
 test "should get about" do
 get :about
