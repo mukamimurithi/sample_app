fd867f63c29d8aab782f6242041d2b98f66e442f
carol <carolmurithi1994@gmail.com>
2016-07-28 15:48:43 +0300

    learn the page layout using CSS

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class applicationHelperTest <ActionView::Signup 
+	test "full title helper" do
+		assert_equal full_title,FILL IN
+		assert_equal full_title("Help"),FILL IN
+		end
+		
+	end
