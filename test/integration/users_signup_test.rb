2346ba3ffd77775507e063f426dfb4dc7c5ebd35
carol <carolmurithi1994@gmail.com>
2016-08-02 12:09:09 +0300

    finish signup page

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+ test "invalid signup information" do
+get signup_path
+assert_difference 'User.count', 1 do
+post_via_redirect users_path, user: { name: "carol mukami",
+email: "carolmurithi1994@gmail.com",
+password:
+"carolmukami",
+password_confirmation: "carolmukami" }
+end
+assert_template 'users/show'
+end
 end
