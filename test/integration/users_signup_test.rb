2f1a29da11ebd27643c823e23b0d13fcb37638af
carol <carolmurithi1994@gmail.com>
2016-08-24 08:54:53 +0300

    UserMailer activation and testing

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,7 +10,10 @@ post users_path, params: {user: { name: "",
                                  }
 end
 assert_template 'users/new'
+assert_select 'div#error_explanation'
+assert_select 'div.field_with_errors'
 end
+
 test "valid signup information" do
 get signup_path
 assert_difference 'User.count', 1 do
@@ -21,9 +24,10 @@ post users_path, params: { user: { name: "carol mukami",
 
                                  }
 
+
 end
-follow_redirect!
-assert_template 'users/show'
+# assert_template 'users/show'
+# assert is_logged_in?
 end
 
 end
