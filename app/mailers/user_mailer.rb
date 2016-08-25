6f09a2e3ca682a0b66d89bab4a26e23cd6e005fd
carol <carolmurithi1994@gmail.com>
2016-08-25 16:55:24 +0300

    password reseting

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,20 +3,9 @@ class UserMailer < ApplicationMailer
     @user = user
     mail to: user.email, subject: "Account activation"
   end
-  def password_reset
-    @greeting = "Hi"
-    mail to: "to@example.org"
+
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
   end
 end
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.account_activation.subject
-
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-attr_accessor :remember_token, :activation_token
+attr_accessor :remember_token, :activation_token, :reset_token
 before_save   :downcase_email
 before_create :create_activation_digest
-validates :name, presence: true, length: { maximum: 50 }
+validates :name, presence: true, length: {minimum: 3, maximum: 50 }
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 validates :email, presence: true, length: { maximum: 255 },
 format: { with: VALID_EMAIL_REGEX },
@@ -45,17 +45,41 @@ end
 
  # Creates and assigns the activation token and digest.
  def create_activation_digest
- self.activation_token = User.new_token
- self.activation_digest = User.digest(activation_token)
+	 self.activation_token = User.new_token
+	 self.activation_digest = User.digest(activation_token)
  end
 
  def activate
-update_attribute(:activated, true)
-update_attribute(:activated_at, Time.zone.now)
+	update_attribute(:activated, true)
+	update_attribute(:activated_at, Time.zone.now)
 end
+
 # Sends activation email.
 def send_activation_email
-UserMailer.account_activation(self).deliver_now
+     UserMailer.account_activation(self).deliver_now
+ end
+
+ # Sets the password reset attributes.
+def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+end
+
+# Sends password reset email.
+def send_password_reset_email
+   UserMailer.password_reset(self).deliver_now
+end
+
+private
+# Converts email to all lower-case.
+def downcase_email
+    self.email = email.downcase
+end
+# Creates and assigns the activation token and digest.
+def create_activation_digest
+   self.activation_token = User.new_token
+   self.activation_digest = User.digest(activation_token)
  end
 end
 
